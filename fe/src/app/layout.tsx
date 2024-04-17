de955d43b219ff0f06d82c7d1ef79773345b0d38
Misheel burenzul <23LP7177@G401-CC0090.local>
2024-04-17 19:41:12 +0800

    homepage blog added

--- fe/src/app/layout.tsx
+++ fe/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ReactNode } from "react";
-
+import "./globals.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">

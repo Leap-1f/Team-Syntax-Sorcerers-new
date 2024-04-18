44f5827b5c83dd4c6134aee120c5fc24d6dbcc9d
Myagmarsuren Buyanjargal <23LP9283@G304-CC0026.local>
2024-04-18 19:16:58 +0800

    cloudinary

--- be/src/controller/user.ts
+++ be/src/controller/user.ts
@@ -1,18 +1,31 @@
 import { UserModel } from "../../models/User.model";
 interface User {
-  _id: string;
-  userName: string;
-  email: number;
-  phoneNumber: string;
-  password: string;
-  address: string;
-  zipCode: number;
-  cartId: string;
-  createdAt: Date;
-  updatedAt: Date;
+  body: {
+    _id: string;
+    userName: string;
+    email: number;
+    phoneNumber: string;
+    password: string;
+    address: string;
+    zipCode: number;
+    cartId: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
 }
+// export interface TypedRequestBody<type> extends Express.Request {
+//   body: type;
+// // }
+// // const requestBody: User = {
+// //   name: "John Doe",
+// //   age: 30,a
+// // };
 export const addUser = async (req: User, res: Express.Response) => {
+  const { userName, email, phoneNumber } = req.body;
+
   try {
+    console.log(userName);
+
     const user = UserModel.create();
   } catch (err) {
     console.error(err);

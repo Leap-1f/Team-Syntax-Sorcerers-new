69f6dfe67d090669c2c07ba5e2254656cc24a55a
purevsambuu <purevsambuu@purevsambuus-MacBook-Air.local>
2024-04-19 09:50:49 +0800

    F

--- fe/src/app/features/page.tsx
+++ fe/src/app/features/page.tsx
@@ -2,11 +2,14 @@
 import { Home } from "./components/Home";
 import { Container2 } from "./components/Container2";
 import { Container3 } from "./components/Container3";
-import { Container4 } from "./components/Container4";
 const Features = () => {
   return (
     <>
-      <Home />
+      <div className="flex flex-col gap-[70px]  ">
+        <Home />
+        <Container2 />
+        <Container3/>
+      </div>
     </>
   );
 };

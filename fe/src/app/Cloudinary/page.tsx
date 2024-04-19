2ca64cf4e741f5ed44a9bd6f1e54c9481cb98f97
Myagmarsuren Buyanjargal <23LP9283@G304-CC0026.local>
2024-04-19 20:10:50 +0800

    done cloudinary

--- fe/src/app/Cloudinary/page.tsx
+++ fe/src/app/Cloudinary/page.tsx
@@ -1,65 +1,43 @@
 "use client";
 import { useState } from "react";
-import {Cloudinary} from "@cloudinary/url-gen";
+import axios from "axios";
 
-
- 
-
-const App = () => {
-  const cld = new Cloudinary({cloud: {cloudName: 'dcwgzeo3g'}});
-};
 const Page = () => {
-  const [imageSrc, setImageSrc] = useState();
-  const [uploadData, setUploadData] = useState();
-  const handleOnChange = (changeEvent: any) => {
-    const reader = new FileReader();
-    reader.onload = function (onLoadEvent: any) {
-      setImageSrc(onLoadEvent.target?.result);
-      setUploadData(undefined);
-    };
-    reader.readAsDataURL(changeEvent.target.files[0]);
+  const [file, setFile] = useState(null);
+  const [filename, setFilename] = useState("");
+
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+    setFilename(event.target.files[0].name);
   };
-  async function handleOnSubmit(event: any) {
+
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
-    const form = event.currentTarget;
-    const fileInput : any= Array.from(form.elements).find(
-      ({ name }: any) => name === "file"
-    );
-    const formData = new FormData();
-    for (const file of fileInput.files) {
-      formData.append("file", file);
+    const formData: any = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "first-test");
+    console.log(file);
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dcwgzeo3g/image/upload",
+        formData
+      );
+      console.log(response);
+    } catch (error) {
+      console.error(error);
     }
-    formData.append("upload_preset", "my-uploads");
-    const data = await fetch(
-      "https://api.cloudinary.com/v1_1/dcwgzeo3g/image/upload",
-      { method: "POST", body: formData }
-    ).then((r) => r.json());
-    setImageSrc(data.secure_url);
-    setUploadData(data);
-    console.log("data", data);
-  }
-  const sendImg =()=>{
-  
-  }
- 
+  };
   return (
-    <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+    <form method="post" onChange={handleFileChange} onSubmit={handleSubmit}>
       <p>
         <input type="file" name="file" />
       </p>
       <div>
-        <img src={imageSrc} alt="zurag" className="w-screen h-screen" />
+        <img src={filename} alt="zurag" className="w-screen h-screen" />
       </div>
-      {imageSrc && !uploadData && (
-        <p>
-          <button>Upload files</button>
-        </p>
-      )}
-      {uploadData && (
-        <code>
-          <pre>{JSON.stringify(uploadData, null, 2)}</pre>
-        </code>
-      )}
+      <p>
+        <button type="submit">a</button>
+      </p>
     </form>
   );
 };

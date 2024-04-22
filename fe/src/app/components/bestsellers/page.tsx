93b7465cc3ba5c6ceb9a37584ebeb31e65fb7ead
Purevsambuu Bukhbayar <23LP4405@G304-CC0055.local>
2024-04-22 19:06:48 +0800

    fdsa

--- fe/src/app/components/bestsellers/page.tsx
+++ fe/src/app/components/bestsellers/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+import React, { useState } from "react";
 export const BestSellers = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div className="w-[100vw] h-[90vh] flex flex-col items-center bg-white gap-[50px]">
       <div className="flex flex-col justify-center items-center gap-[30px] ">
@@ -8,43 +11,51 @@ export const BestSellers = () => {
           sem primis eget maecenas sed urna malesuada.
         </p>
 
-        <div className="flex gap-[1px]  font-semibold">
-          <div className="w-[140px] h-[50px] border-2 border-gray-400  flex justify-center items-center">
+        <div className="flex gap-[1px]  font-semibold cursor-pointer">
+          <div className="w-[140px] h-[50px] border-2 border-gray-400 duration-200  flex justify-center items-center hover:border-[#2BB9A9] hover:text-[#2BB9A9] hover:duration-200">
             All Shoes
           </div>
-          <div className="w-[140px] h-[50px] border-2 border-gray-400  flex justify-center items-center">
+          <div className="w-[140px] h-[50px] border-2 border-gray-400 duration-200  flex justify-center items-center hover:border-[#2BB9A9] hover:text-[#2BB9A9] hover:duration-200">
             Men's
           </div>
-          <div className="w-[140px] h-[50px] border-2 border-gray-400  flex justify-center items-center">
+          <div className="w-[140px] h-[50px] border-2 border-gray-400 duration-200  flex justify-center items-center hover:border-[#2BB9A9] hover:text-[#2BB9A9] hover:duration-200">
             Women's
           </div>
-          <div className="w-[140px] h-[50px] border-2 border-gray-400  flex justify-center items-center">
+          <div className="w-[140px] h-[50px] border-2 border-gray-400 duration-200  flex justify-center items-center hover:border-[#2BB9A9] hover:text-[#2BB9A9] hover:duration-200">
             Kids
           </div>
         </div>
       </div>
       <div className="flex items-center justify-between w-[1400px] h-[360px]">
-        <div className="w-[330px] h-[460px] flex flex-col justify-center items-center hover:shadow-xl ">
+        <div
+          className="w-[330px] h-[460px] flex flex-col justify-center items-center group group-hover:rotate-3 group-hover:scale-125 hover:shadow-xl duration-300"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
           <div
             className="w-[250px] h-[300px] "
             style={{
               padding: "30px",
-
-              backgroundImage:
-                "url('https://wphix.com/template/gota/gota/assets/img/product/6.jpg')",
+              backgroundImage: `url('${
+                isHovered
+                  ? "https://wphix.com/template/gota/gota/assets/img/product/6.jpg"
+                  : "https://wphix.com/template/gota/gota/assets/img/product/7.jpg"
+              }')`,
               backgroundSize: "cover",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
             }}
           ></div>
-          <div className="flex flex-col w-[310px]  h-[100px] gap-[15px]">
-            <div className="flex justify-between  items-center">
+          <div className="flex flex-col w-[310px] h-[100px] gap-[15px]">
+            <div className="flex justify-between items-center">
               <p className="text-[12px] text-gray-400">SHOES,CLOTHING</p>
               <div className="">star</div>
             </div>
             <div className="border border-gray-400 w-[310px]"></div>
             <div className="text-[16px] font-semibold tracking-tight flex flex-col gap-[10px]">
-              <h1>Flat Sandal For Woman</h1>
+              <h1 className={isHovered ? "" : ""}>
+                {isHovered ? "Add to card" : "Flat Sandal For Woman"}
+              </h1>
               <p>$660.000-$680.000</p>
             </div>
           </div>

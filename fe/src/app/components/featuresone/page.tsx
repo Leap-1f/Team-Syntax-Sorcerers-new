82203bd3a836abd85e18d263904334c10b466558
tuvshuu <148052892+fenrir434@users.noreply.github.com>
2024-04-22 18:44:45 +0800

    ez carousel

--- fe/src/app/components/featuresone/page.tsx
+++ fe/src/app/components/featuresone/page.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "@mui/material";
+import React from "react";
 
 export const FeaturesOne = () => {
   return (
--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,16 +1,18 @@
 import Header from "./components/header/page";
 import Footer from "./components/footer/page";
-import { Card } from "@/app/components/card/page";
 import { Stack } from "@mui/material";
 import { FeaturesOne } from "./components/featuresone/page";
 import { FeaturesTwo } from "./components/featuresTwo/page";
 import { Blog } from "./components/blog/page";
+import React from "react";
+import Carousel from "./components/carousel/page";
 
 export default function Home() {
   return (
     <>
       <Stack direction={"column"} alignItems={"center"}>
         <Header />
+        <Carousel />
         {/* Doorh Stack-d FeaturesOne, Best seller components orno. */}
         <Stack width={"1400px"} direction={"column"}>
           <FeaturesOne />

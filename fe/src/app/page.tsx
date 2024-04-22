2495d84d15d11a3f31b464a1463c1102edb80642
Аригун Bold <23LP5891@iMAC2021-05s-iMac.local>
2024-04-22 13:47:52 +0800

    Design almost done, hover dutuu

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "./components/footer/page";
 import { Card } from "@/app/components/card/page";
 import { Stack } from "@mui/material";
 import { FeaturesOne } from "./components/featuresone/page";
+import { Blog } from "./components/blog/page";
 export default function Home() {
   return (
     <>
@@ -12,7 +13,9 @@ export default function Home() {
         <FeaturesOne />
       </Stack>
       {/* Doorh Stack-d Blog, GetInTouch components orno. */}
-      <Stack width={"1400px"} direction={"column"}></Stack>
+      <Stack width={"1400px"} direction={"column"} alignItems={"center"}>
+        <Blog />
+      </Stack>
       <Footer />
     </>
   );

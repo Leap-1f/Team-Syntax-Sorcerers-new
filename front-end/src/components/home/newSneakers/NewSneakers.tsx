cea0f91e5d3d24639b85996d70a2d53b399c57f7
tuvshuu <148052892+fenrir434@users.noreply.github.com>
2024-04-26 17:49:22 +0800

    added animate on scroll
    apr.26

--- front-end/src/components/home/newSneakers/NewSneakers.tsx
+++ front-end/src/components/home/newSneakers/NewSneakers.tsx
@@ -1,6 +1,20 @@
+"use client";
+import React, { use, useEffect } from "react";
 import { Stack, Typography } from "@mui/material";
 import { Card } from "./Card";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export const FeaturesTwo = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1200,
+            easing: "ease",
+            once: false,
+        });
+        AOS.refresh(); // Refresh AOS after initialization
+    }, []);
+
     const featureTypoGraphy = {
         color: "#FFF",
         textAlign: "center",
@@ -37,6 +51,7 @@ export const FeaturesTwo = () => {
                     paddingTop: "60px",
                     paddingBottom: "80px",
                 }}
+                data-aos="fade-up"
             >
                 <div
                     style={{
@@ -68,10 +83,10 @@ export const FeaturesTwo = () => {
                     justifyContent={"center"}
                     spacing={"40px"}
                 >
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    <Card imageUrl="https://res.cloudinary.com/dqhy9ufze/image/upload/v1714049024/d0f78259b152acea1a2a227ce946c439_pjp79c.jpg" index={0} />
+                    <Card imageUrl="https://res.cloudinary.com/dqhy9ufze/image/upload/v1714049019/d18e34eb34c9a364f0ca7ef6a8076f50_ns3j84.jpg" index={0} />
+                    <Card imageUrl="https://res.cloudinary.com/dqhy9ufze/image/upload/v1714036836/7_scirkj.jpg" index={0} />
+                    <Card imageUrl="https://res.cloudinary.com/dqhy9ufze/image/upload/v1714036831/6_g79t15.jpg" index={0} />
                 </Stack>
             </Stack>
         </>

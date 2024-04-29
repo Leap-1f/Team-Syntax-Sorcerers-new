e0cebf1d6721061de0a82701adccde971b08de70
Zes-Erdene <150428293+Zes-Erdene@users.noreply.github.com>
2024-04-29 17:12:15 +0800

    zesee

--- front-end/src/app/aboutUs/page.tsx
+++ front-end/src/app/aboutUs/page.tsx
@@ -1,4 +1,289 @@
-const AboutUs = () => {
-  return <></>;
-};
-export default AboutUs;
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import EmojiEventsRoundedIcon from "@mui/icons-material/EmojiEventsRounded";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+export default function AboutUs() {
+  return (
+    <>
+      <div className="flex w-[100vw]">
+        <div className="flex-col">
+          <div className="w-[100vw] h-[620px] bg-black ">
+            <h1 className="text-white text-center pt-[12%] text-6xl ">
+              ABOUT US
+            </h1>
+            <div className="flex justify-center gap-8 mt-8">
+              <button className="text-white text-center">Home</button>
+              <button className="text-white text-center">About</button>
+            </div>
+          </div>
+          <div className="flex justify-center mt-16 ">
+            <div className="flex-col">
+              <div className="w-[100vw]">
+                <div className="flex flex-col w-[100vw] items-center">
+                  <div className="flex-col mr-80">
+                    <p className="text-[#999999] font-normal text-base mb-3">
+                      Our History
+                    </p>
+
+                    <div className="w-[100px] h-[2px] bg-black"></div>
+                    <h3 className="text-[#222222] font-semibold text-4xl leading-10 mt-8">
+                      We Are A Lifestyle Brand
+                    </h3>
+                    <div className="flex-col  gap-6 mt-14 leading-[40px]">
+                      <p>
+                        It is accompanied by a case that can contain up to three
+                        different diffusers and can be used for dry storage of
+                        loose tea. The perfect way to enjoy brewing tea on low
+                        hanging fruit to identify. Duis
+                        <br />
+                        autem vel eum iriure dolor in hendrerit in vulputate
+                        velit esse molestie consequat, velillum dolore eu
+                        feugiat nulla facilisis.
+                      </p>
+                      <p>
+                        The perfect way to enjoy brewing tea on low hanging
+                        fruit to identify. Duis autem vel eum iriure dolor in
+                        hendrerit in vulputate velit esse molestie consequat,
+                        vel illum dolore eu feugiat nulla facilisis. <br />
+                        For me, the most important part of improving at
+                        photography has been sharing it.
+                      </p>
+                      <p>
+                        Sign up for an Exposure account, or post regularly to
+                        Tumblr, or both. Tell people you’re trying to get better
+                        at photography. Talk about it. When you talk about it,
+                        other people get excited about it.
+                        <br />
+                        There are few plugins and apps available for this
+                        purpose, many of them required a monthly subscription or
+                        needed to expose the full store data to a third party.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="mt-20 bg-white grid grid-cols-3 grid-rows-3 gap-2">
+                    <div className=" col-span-2 row-span-2 ">
+                      <img
+                        className="w-[890px] h-[508]"
+                        src=" https://i.ibb.co/pdNjrWq/image1-about-jpg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className=" col-span-1 row-span-1">
+                      <img
+                        className="w-[442px] h-[273px]"
+                        src=" https://i.ibb.co/XFdn1q9/image4-about-jpg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className=" row-span-2 col-span-1 ">
+                      <img
+                        className="w-[442px] h-[566px]"
+                        src=" https://i.ibb.co/LvQVx6t/image5-about-jpg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className=" row-span-1 col-span-1">
+                      <img
+                        className="w-[442px] h-[273px]"
+                        src=" https://i.ibb.co/Cw5vCb8/image2-about-jpg.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className=" row-span-1 col-span-1">
+                      <img
+                        className="w-[442px] h-[273px]"
+                        src=" https://i.ibb.co/M766DMv/image3-about-jpg.png"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex gap-14 mt-14 justify-center pt-10 pb-20">
+                  <div className="flex-col flex gap-2">
+                    <div className="flex justify-center">
+                      <EmojiEventsRoundedIcon />
+                    </div>
+                    <h3 className="text-[#222222] text-2xl font-semibold text-center">
+                      19 Years of <br /> Experience
+                    </h3>
+                    <p>
+                      Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem
+                      <br />
+                      accusantium doloremque laudantium, totam rem aperiam,
+                      eaque ipsa
+                      <br />
+                      quae ab illo inventore veritatis.
+                    </p>
+                  </div>
+                  <div className="flex-col flex gap-2">
+                    <div className="flex justify-center">
+                      <ThumbUpOutlinedIcon />
+                    </div>
+
+                    <h3 className="text-[#222222] text-2xl font-semibold text-center">
+                      Awesome <br /> Performance
+                    </h3>
+                    <p>
+                      Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem
+                      <br />
+                      accusantium doloremque laudantium, totam rem aperiam,
+                      eaque ipsa
+                      <br />
+                      quae ab illo inventore veritatis.
+                    </p>
+                  </div>
+
+                  <div className="flex-col flex gap-2">
+                    <div className="flex justify-center">
+                      <Person2OutlinedIcon />
+                    </div>
+
+                    <h3 className="text-[#222222] text-2xl font-semibold text-center">
+                      60,000+ Happy <br /> Customer
+                    </h3>
+                    <p>
+                      Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem
+                      <br />
+                      accusantium doloremque laudantium, totam rem aperiam,
+                      eaque ipsa
+                      <br />
+                      quae ab illo inventore veritatis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[100vw] h-[670px] bg-black flex-col">
+                <h1 className="text-white text-center pt-[12%] text-6xl ">
+                  All Orders Are Free <br /> Shipping
+                </h1>
+                <p className="text-white text-center text-sm font-normal mt-3">
+                  Donec vehicula cursus vestibulum lectus, sit eros integer
+                  varius cum turpis et quam congue <br />
+                  accumsan ac bibendum ac in erat. Donec posuere consectetuer
+                  volutpat rutrum ac sollicitudin quam quisque at interdum
+                  dignissim fringilla <br />
+                  elit risus lorem condimentum eros mollis.
+                </p>
+              </div>
+              <div className="flex w-[100vw] h-[800px] bg-white justify-center">
+                <div className="w-[1400px] flex flex-col  items-center mt-10">
+                  <p className="text-[#999999]  font-normal text-base ">
+                    Our Team
+                  </p>
+                  <div className="w-[100px] h-[2px] bg-black mt-5"></div>
+                  <h3 className=" text-4xl font-semibold mt-5">Team Members</h3>
+                  <p className="text-[#888888]  text-sm font-normal mt-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                  <div className="flex pt-[100px] gap-[150px]">
+                    <div className="flex-col ">
+                      <img
+                        className="w-[300px] h-[300px]"
+                        src=" https://i.ibb.co/h8jNsS1/asset6-png.png"
+                        alt=""
+                      />
+                      <h2 className="text-xl font-semibold text-center mt-5">
+                        Eren <br /> Christopher
+                      </h2>
+                      <p className="text-sm font-normal text-center  text-[#999]">
+                        Founder or Agota Store
+                      </p>
+                    </div>
+                    <div className="flex-col">
+                      <img
+                        className="w-[300px] h-[300px]"
+                        src="https://i.ibb.co/N32tYB1/asset7-png.png"
+                        alt=""
+                      />
+                      <h2 className="text-xl font-semibold text-center mt-5">
+                        Mike <br /> Jefferson
+                      </h2>
+                      <p className="text-sm font-normal text-center  text-[#999]">
+                        Founder or Agota Store
+                      </p>
+                    </div>
+                    <div className="flex-col">
+                      <img
+                        className="w-[300px] h-[300px]"
+                        src="https://i.ibb.co/r5GBJ9k/asset8-png.png"
+                        alt=""
+                      />
+                      <h2 className="text-xl font-semibold text-center mt-5">
+                        Eren <br /> Christopher
+                      </h2>
+                      <p className="text-sm font-normal text-center  text-[#999]">
+                        Founder or Agota Store
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-[100vw] h-[677px] bg-[#97545B] justify-center ">
+                <div className="w-[1396px] h-[393px] flex flex-col bg-[#97545B] mt-24  items-center pt-[40px]">
+                  <img
+                    className=" w-[40px] h-[40px]"
+                    src="https://i.ibb.co/F68QHxY/Icon.png"
+                    alt=""
+                  />
+                  <img
+                    className=" w-[80px] h-[80px] mt-5"
+                    src="https://i.ibb.co/F8481kY/asset9-png.png"
+                    alt=""
+                  />
+
+                  <p className="text-white  text-sm font-normal mt-2 text-center leading-8">
+                    For me the most important part of improving at photography
+                    has been sharing it. <br />
+                    Sign up for an Exposure account, or post regularly to
+                    Tumblr, or both. Tell people you’re trying to get <br />
+                    better at photography. Minimalist approach, bright colors,
+                    stainless steel and matte plastic, abstract <br />
+                    shapes and curved lines are the defining features of these{" "}
+                    <br />
+                    products designed to be extremely functional, user-friendly
+                    and fun.
+                  </p>
+                  <h3 className="text-white text-base font-normal text-center">
+                    Eren <br /> Christopher
+                  </h3>
+                </div>
+              </div>
+              <div className="w-[100vw] h-[160px] flex justify-evenly items-center ">
+                <img
+                  className=" w-[97px] h-[62px]"
+                  src="https://i.ibb.co/s5yfhWj/8-asset10-png.png"
+                  alt=""
+                />
+                <img
+                  className=" w-[97px] h-[62px]"
+                  src="https://i.ibb.co/7SPqwFx/8-asset11-png.png"
+                  alt=""
+                />
+                <img
+                  className=" w-[97px] h-[62px]"
+                  src=" https://i.ibb.co/PcdGzrB/8-asset12-png.png"
+                  alt=""
+                />
+                <img
+                  className=" w-[97px] h-[62px]"
+                  src="https://i.ibb.co/cDGrDDH/8-asset13-png.png"
+                  alt=""
+                />
+                <img
+                  className=" w-[97px] h-[62px]"
+                  src="https://i.ibb.co/VjdBcWR/8-asset14-png.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

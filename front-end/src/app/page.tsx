7c6f5553504269522267add5b06edc8039219393
tuvshuu <148052892+fenrir434@users.noreply.github.com>
2024-05-03 18:39:02 +0800

    BestSellers2
    bestsellersiin top limited har gutaliig hiiv

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -5,6 +5,7 @@ import FeaturesOne from "../components/home/hotCards/HotCards";
 import FeaturesTwo from "../components/home/newSneakers/NewSneakers";
 import Blog from "../components/home/Blog";
 import BestSellers from "../components/home/BestSellers";
+import BestSellers2 from "../components/home/BestSellers2";
 import Carousel2 from "../components/home/Carousel2";
 import AboutOurTeam from "../components/home/AboutOurTeam";
 import OurService from "../components/home/OurService";
@@ -12,25 +13,34 @@ import GetInTouch from "../components/home/GetInTouch";
 import { Stack } from "@mui/material";
 
 const Home: React.FC = () => {
-  return (
-    <>
-      <Stack direction={"column"} alignItems={"center"}>
-        <Header />
-        <Carousel2 />
-        <Stack width={"1400px"} direction={"column"} alignItems={"center"}>
-          <FeaturesOne />
-          <AboutOurTeam />
-          <OurService />
-        </Stack>
-        <BestSellers />
-        <FeaturesTwo />
-        <Stack width={"1400px"} direction={"column"} alignItems={"center"}>
-          <Blog />
-        </Stack>
-        <GetInTouch />
-        <Footer />
-      </Stack>
-    </>
-  );
+    return (
+        <>
+            <Stack direction={"column"} alignItems={"center"}>
+                <Header />
+                <Carousel2 />
+                <Stack
+                    width={"1400px"}
+                    direction={"column"}
+                    alignItems={"center"}
+                >
+                    <FeaturesOne />
+                    <AboutOurTeam />
+                    <OurService />
+                </Stack>
+                <BestSellers2 />
+                <BestSellers />
+                <FeaturesTwo />
+                <Stack
+                    width={"1400px"}
+                    direction={"column"}
+                    alignItems={"center"}
+                >
+                    <Blog />
+                </Stack>
+                <GetInTouch />
+                <Footer />
+            </Stack>
+        </>
+    );
 };
 export default Home;

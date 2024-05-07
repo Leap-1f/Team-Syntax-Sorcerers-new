3e49a6e9a85b82f294fdfc32e3848ecb267962dd
Аригун Bold <23LP5891@administrators-iMac-3.local>
2024-05-07 11:37:19 +0800

    New shop page Ui done

--- front-end/src/components/shop/ContainerRight/Sales.tsx
+++ front-end/src/components/shop/ContainerRight/Sales.tsx
@@ -16,8 +16,25 @@ import { FaArrowDown } from "react-icons/fa";
 import { FaArrowUp } from "react-icons/fa";
 import Image from "next/image";
 
+type TCheckedBox = {
+  name: string;
+  label: string;
+};
+const checkboxes: TCheckedBox[] = [
+  { name: "Lebron", label: "Label" },
+  { name: "Tatum", label: "Required" },
+  { name: "Jordan", label: "Disabled" },
+];
+
 export const Sales = () => {
   const [getProductData, setGetProductData] = useState<any[]>([]);
+  const [expanded, setExpanded] = useState(false);
+  const [checked, setChecked] = useState({
+    label: true,
+    required: false,
+    disabled: false,
+  });
+
   useEffect(() => {
     const fetchProducts = async () => {
       const data = await getProducts();
@@ -25,9 +42,21 @@ export const Sales = () => {
     };
     fetchProducts();
   }, []);
+
+  /* ///////////////////// HANDLER  //////////////////////////*/
+  const handleClick = () => {
+    console.log("Typography clicked!");
+  };
+  const handleChange = (event: any) => {
+    setChecked({ ...checked, [event.target.name]: event.target.checked });
+  };
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  /* ///////////////////// TYPO  //////////////////////////*/
   const categoryTypo = {
     flex: "1 1 0%",
-    font: "var(--podium-cds-typography-body1-strong)",
     fontSize: "24px",
     fontWeight: 700,
     lineHeight: "1.3",
@@ -56,31 +85,29 @@ export const Sales = () => {
   const pxZeroTypo = {
     py: "0px",
   };
+  const pruductCardSubtitle = {
+    color: "#707072",
+    fontSize: "16px",
+  };
+  const productCardTitle = {
+    color: "#111111",
+    fontWeight: 600,
+    fontSize: "16px",
+    lineHeight: "1.5",
+    fontFamily: "'Helvetica Now Text Medium', Helvetica, Arial, sans-serif",
+  };
+
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
+    width: "100%",
+    height: "400px",
+    position: "relative",
   }));
-  const handleClick = () => {
-    console.log("Typography clicked!");
-  };
 
-  const [checked, setChecked] = useState({
-    label: true,
-    required: false,
-    disabled: false,
-  });
-  const [expanded, setExpanded] = useState(false);
-
-  const handleChange = (event: any) => {
-    setChecked({ ...checked, [event.target.name]: event.target.checked });
-  };
-
-  const handleToggle = () => {
-    setExpanded(!expanded);
-  };
   return (
     <Stack width={"100%"} spacing={"20px"}>
       <Stack width={"100%"}>
@@ -105,18 +132,15 @@ export const Sales = () => {
             sx={{ borderBottom: "2px solid #E5E5E5" }}
             paddingBottom={"20px"}
           >
-            <Typography sx={categorySortTypo} onClick={handleClick}>
-              Lebron
-            </Typography>
-            <Typography sx={categorySortTypo} onClick={handleClick}>
-              Curry
-            </Typography>
-            <Typography sx={categorySortTypo} onClick={handleClick}>
-              Tatum
-            </Typography>
-            <Typography sx={categorySortTypo} onClick={handleClick}>
-              Harden
-            </Typography>
+            {checkboxes.map((categoryName, index) => (
+              <Typography
+                key={index}
+                sx={categorySortTypo}
+                onClick={handleClick}
+              >
+                {categoryName.name}
+              </Typography>
+            ))}
           </Stack>
           <Typography
             sx={{
@@ -145,108 +169,58 @@ export const Sales = () => {
                 },
               }}
             >
-              <FormControlLabel
-                sx={sortTypo}
-                control={
-                  <Checkbox
-                    checked={checked.label}
-                    onChange={handleChange}
-                    name="label"
-                    sx={pxZeroTypo}
-                  />
-                }
-                label="Label"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={checked.required}
-                    onChange={handleChange}
-                    name="required"
-                    sx={pxZeroTypo}
-                  />
-                }
-                label="Required"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={checked.disabled}
-                    onChange={handleChange}
-                    name="disabled"
-                    sx={pxZeroTypo}
-                  />
-                }
-                label="Disabled"
-              />
+              {checkboxes.map((checkbox, index) => (
+                <FormControlLabel
+                  key={index}
+                  control={
+                    <Checkbox
+                      onChange={handleChange}
+                      name={checkbox.label}
+                      sx={pxZeroTypo}
+                    />
+                  }
+                  label={checkbox.name}
+                />
+              ))}
             </FormGroup>
           )}
         </Stack>
-        {/* <Stack width={"80%"} bgcolor={"red"} paddingLeft={"30px"}></Stack> */}
-        {/* <Grid width={"80%"} bgcolor={"red"} paddingLeft={"30px"}>
-          dadas
-        </Grid> */}
         <Box sx={{ flexGrow: 1 }}>
           <Grid
             container
             spacing={{ xs: 2, md: 3 }}
-            columns={{ xs: 4, sm: 8, md: 12 }}
+            columns={{ xs: 2, sm: 8, md: 12 }}
           >
             {Array.from(Array(6)).map((_, index) => (
-              <Grid item xs={2} sm={4} md={4} key={index}>
+              <Grid item xs={2} sm={4} md={4} key={index} width={"600px"}>
                 <Item>
                   <Image
-                    className="w-full"
+                    className="w-full h-[full] top-0 left-0 object-cover rounded-2xl"
+                    alt="profile"
+                    objectFit="cover"
                     fill
                     src={
                       "https://res.cloudinary.com/dqhy9ufze/image/upload/v1714036836/7_scirkj.jpg"
                     }
-                    alt=""
-                  ></Image>
+                  />
                 </Item>
+                <Stack paddingTop={"20px"}>
+                  <Typography sx={productCardTitle}>
+                    Tatum 2 'Legacy' PF
+                  </Typography>
+                  <Typography sx={pruductCardSubtitle}>
+                    Basketball Shoes
+                  </Typography>
+                  <Typography sx={pruductCardSubtitle}>1 Colour</Typography>
+                </Stack>
+                <Typography paddingTop={"10px"} sx={productCardTitle}>
+                  $500
+                </Typography>
               </Grid>
             ))}
           </Grid>
         </Box>
       </Stack>
-
-      {/* <Stack
-        direction={"column"}
-        spacing={"100px"}
-        sx={{ width: "15%", paddingTop: "50px" }}
-      >
-        <ProductCategory />
-        <FilterByPrice />
-        <Size />
-        <ColorOption />
-        <TopRated />
-        <ProductTags />
-      </Stack>
-      <Stack
-        direction={"column"}
-        spacing={"20px"}
-        sx={{ width: "85%", paddingTop: "50px" }}
-      >
-        <Header />
-        <Stack
-          direction={"row"}
-          flexWrap={"wrap"}
-          width={"100%"}
-          height={"100%"}
-        >
-          {getProductData.map((product, index) => (
-            <ProductsEssentialI2
-              key={product._id}
-              imageUrl={product.image}
-              title={product.name}
-              price={product.price}
-              index={product._id}
-              brand={product.brand}
-              category={product.category}
-            />
-          ))}
-        </Stack>
-      </Stack> */}
     </Stack>
   );
 };

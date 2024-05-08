26f62483e1685f02fd12a4c5830b3fe86c9a37fa
Мийгаа <150428221+TheMyagmarsuren@users.noreply.github.com>
2024-05-08 17:36:12 +0800

    z

--- back-end/src/models/order.model.ts
+++ back-end/src/models/order.model.ts
@@ -1,19 +1,19 @@
 import { model, models, Schema } from "mongoose";
 import { COLLECTIONS } from "../constant";
 const orderItemsSChema = new Schema({
-  product: { type: String, ref: COLLECTIONS.PRODUCT },
+  product: { type: [Schema.Types.ObjectId], ref: COLLECTIONS.PRODUCT },
   count: { type: Number, required: true },
 });
 const OrderSchema = new Schema(
   {
-    userId: { type: [Schema.Types.ObjectId], ref: COLLECTIONS.USER },
+    user: { type: Schema.Types.ObjectId, ref: COLLECTIONS.USER },
     status: {
       type: String,
       enum: ["Zahialsan", "Hurgegdsen", "Hurgelted garsan "],
       default: "Zahialsan",
     },
 
-    location: { type: [Schema.Types.ObjectId], ref: COLLECTIONS.LOCATION },
+    location: { type: Schema.Types.ObjectId, ref: COLLECTIONS.LOCATION },
     orderItems: [orderItemsSChema],
   },
   {

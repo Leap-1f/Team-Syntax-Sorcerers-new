71d2f9cf624c7c3eaba525236a93cbcedf581820
tuvshuu <148052892+fenrir434@users.noreply.github.com>
2024-05-08 18:16:55 +0800

    best seller fix

--- front-end/src/components/layout/Header.tsx
+++ front-end/src/components/layout/Header.tsx
@@ -129,7 +129,15 @@ function Header() {
 
     return (
         <>
-            <Stack width={"100%"} position={"fixed"} top={"0"} zIndex={"100"}>
+            <Stack
+                width={"100%"}
+                position={"fixed"}
+                top={"0"}
+                zIndex={"100"}
+                sx={{
+                    boxShadow: "0px 10px 10px rgba(0, 0, 0, 0.25)", // Adjust shadow properties
+                }}
+            >
                 {Bag(showAddCard, openAddCard)}
                 <Stack
                     width={"100%"}

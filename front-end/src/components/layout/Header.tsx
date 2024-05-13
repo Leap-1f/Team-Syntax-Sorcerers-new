5090d48375d9e3e25574760d8b9524f80b76791b
Мийгаа <150428221+TheMyagmarsuren@users.noreply.github.com>
2024-05-13 15:33:28 +0800

    a

--- front-end/src/components/layout/Header.tsx
+++ front-end/src/components/layout/Header.tsx
@@ -7,256 +7,285 @@ import { Bag } from "./myBags";
 import { Grid, Paper, SelectChangeEvent, styled, Button } from "@mui/material";
 
 interface CategoryItemProps {
-    label: string;
-    href: string;
+  label: string;
+  href: string;
 }
 const store = {
-    getSnapshot: () => sessionStorage.getItem("cart") || "[]",
-    subscribe: (listener: () => void) => {
-        window.addEventListener("storage", listener);
-        return () => void window.removeEventListener("storage", listener);
-    },
+  getSnapshot: () => sessionStorage.getItem("cart") || "[]",
+  subscribe: (listener: () => void) => {
+    window.addEventListener("storage", listener);
+    return () => void window.removeEventListener("storage", listener);
+  },
 };
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 const StyledInputBase = styled("input")(({ theme }) => ({
-    color: "inherit",
-    width: "100%",
-    paddingLeft: "20px",
+  color: "inherit",
+  width: "100%",
+  paddingLeft: "20px",
 }));
 const Search = styled("div")({
-    display: "flex",
-    alignItems: "center",
-    borderRadius: "5px",
+  display: "flex",
+  alignItems: "center",
+  borderRadius: "5px",
 });
 const CategoryItem: FC<CategoryItemProps> = ({ label, href }) => (
-    <Button
-        component="a"
-        href={href}
-        variant="text"
-        sx={{
-            width: "fit-content",
-            color: "black",
-            "&:hover": {
-                color: "#2BB9A9",
-            },
-        }}
-    >
-        {label}
-    </Button>
+  <Button
+    component="a"
+    href={href}
+    variant="text"
+    sx={{
+      width: "fit-content",
+      color: "black",
+      "&:hover": {
+        color: "#2BB9A9",
+      },
+    }}
+  >
+    {label}
+  </Button>
 );
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
 }));
 const categoriesLeftSide: CategoryItemProps[] = [
-    {
-        label: "Нүүр хуудас",
-        href: "/",
-    },
-    {
-        label: "Бүх бараа",
-        href: "/shop",
-    },
-    {
-        label: "Мэдээ",
-        href: "/blog",
-    },
+  {
+    label: "Нүүр хуудас",
+    href: "/",
+  },
+  {
+    label: "Бүх бараа",
+    href: "/shop",
+  },
+  {
+    label: "Мэдээ",
+    href: "/blog",
+  },
 ];
 const categoriesRightSide: CategoryItemProps[] = [
-    {
-        label: "Бидний тухай",
-        href: "/aboutUs",
-    },
-    {
-        label: "Үйлчилгээ",
-        href: "/ourService",
-    },
-    {
-        label: "Холбоо барих",
-        href: "/contactus",
-    },
+  {
+    label: "Бидний тухай",
+    href: "/aboutUs",
+  },
+  {
+    label: "Үйлчилгээ",
+    href: "/ourService",
+  },
+  {
+    label: "Холбоо барих",
+    href: "/contactus",
+  },
 ];
 const headerCurrencyTypo = {
-    fontSize: "16px",
-    fontFamily: "Barlow",
-    fontWeight: 500,
-    color: "#adacac",
+  fontSize: "16px",
+  fontFamily: "Barlow",
+  fontWeight: 500,
+  color: "#adacac",
 };
 const gotaTypo = {
-    fontSize: "16px",
-    textTransform: "uppercase",
-    px: "30px",
-    fontWeight: 600,
-    color: "#222222",
+  fontSize: "16px",
+  textTransform: "uppercase",
+  px: "30px",
+  fontWeight: 600,
+  color: "#222222",
 };
 const buttonTypo = {
-    fontSize: "16px",
-    color: "#222222",
-    fontWeight: 600,
-    textTransform: "uppercase",
-    display: "block",
-    paddingRight: "30px",
+  fontSize: "16px",
+  color: "#222222",
+  fontWeight: 600,
+  textTransform: "uppercase",
+  display: "block",
+  paddingRight: "30px",
 };
 function Header() {
-    const [currency, setCurrency] = useState<string>("USD");
-    const handleChange = (event: SelectChangeEvent) => {
-        setCurrency(event.target.value as string);
-    };
-    const [showAddCard, setShowAddCard] = useState(false);
-    const openAddCard = () => {
-        setShowAddCard((prev) => !prev);
-    };
-    const kart: any = useSyncExternalStore(store.subscribe, store.getSnapshot);
-    const wart: any = JSON.parse(kart);
-    return (
-        <>
-            <Stack width={"100%"} position={"sticky"} top={"0"} zIndex={"100"}>
-                {Bag(showAddCard, openAddCard)}
-                <Stack
-                    width={"100%"}
-                    height={"48px"}
-                    bgcolor={"#202020"}
-                    px={"48px"}
-                    direction={"row"}
-                    justifyContent={"space-between"}
-                    alignItems={"center"}
-                    color={"white"}
+  const [currency, setCurrency] = useState<string>("USD");
+  const handleChange = (event: SelectChangeEvent) => {
+    setCurrency(event.target.value as string);
+  };
+  const [showAddCard, setShowAddCard] = useState(false);
+  const openAddCard = () => {
+    setShowAddCard((prev) => !prev);
+  };
+  const kart: any = useSyncExternalStore(store.subscribe, store.getSnapshot);
+  const wart: any = JSON.parse(kart);
+  return (
+    <>
+      <Stack width={"100%"} position={"sticky"} top={"0"} zIndex={"100"}>
+        {Bag(showAddCard, openAddCard)}
+        <Stack
+          width={"100%"}
+          height={"48px"}
+          bgcolor={"#202020"}
+          px={"48px"}
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          color={"white"}
+        >
+          <Stack direction={"row"} color={"#88888"}></Stack>
+          <Stack direction={"row"}>
+            <Button
+              sx={{
+                textTransform: "uppercase",
+                color: "#adacac",
+                "&:hover": {
+                  color: "#2bb9a9",
+                },
+              }}
+            >
+              Бүртгүүлэх
+            </Button>
+            <Button
+              sx={{
+                textTransform: "uppercase",
+                color: "#adacac",
+                "&:hover": {
+                  color: "#2bb9a9",
+                },
+              }}
+            >
+              Нэвтрэх
+            </Button>
+          </Stack>
+        </Stack>
+        <Grid
+          container
+          alignItems="center"
+          justifyContent={"space-between"}
+          bgcolor={"white"}
+        >
+          <Grid item sx={{ width: "20%" }}></Grid>
+          <Grid
+            item
+            direction={"row"}
+            justifyContent={"center"}
+            sx={{ width: "60%" }}
+          >
+            <Stack
+              width={"100%"}
+              direction={"row"}
+              justifyContent={"space-evenly"}
+              sx={{ py: "30px", px: "48px" }}
+              spacing={"10px"}
+            >
+              {categoriesLeftSide.map((category, index) => (
+                <Button
+                  sx={{
+                    ...buttonTypo,
+                    direction: "flex",
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "center",
+                    "&:hover": {
+                      backgroundColor: "none",
+                      border: "none !important",
+                      color: "#2bb9a9",
+                    },
+                  }}
+                  key={index}
+                  href={category.href}
                 >
-                    <Stack direction={"row"} color={"#88888"}></Stack>
-                    <Stack direction={"row"}>
-                        <Button
-                            sx={{
-                                textTransform: "uppercase",
-                                color: "#adacac",
-                                "&:hover": {
-                                    color: "#2bb9a9",
-                                },
-                            }}
-                        >
-                            Бүртгүүлэх
-                        </Button>
-                        <Button
-                            sx={{
-                                textTransform: "uppercase",
-                                color: "#adacac",
-                                "&:hover": {
-                                    color: "#2bb9a9",
-                                },
-                            }}
-                        >
-                            Нэвтрэх
-                        </Button>
-                    </Stack>
-                </Stack>
-                <Grid
-                    container
-                    alignItems="center"
-                    justifyContent={"space-between"}
-                    bgcolor={"white"}
+                  {category.label}
+                </Button>
+              ))}
+              <a href="/">
+                {" "}
+                {/* Adjust the href accordingly */}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
                 >
-                    <Grid item sx={{ width: "20%" }}></Grid>
-                    <Grid
-                        item
-                        direction={"row"}
-                        justifyContent={"center"}
-                        sx={{ width: "60%" }}
-                    >
-                        <Stack
-                            width={"100%"}
-                            direction={"row"}
-                            justifyContent={"space-evenly"}
-                            sx={{ py: "30px", px: "48px" }}
-                            spacing={"10px"}
-                        >
-                            {categoriesLeftSide.map((category, index) => (
-                                <Button
-                                    sx={{
-                                        ...buttonTypo,
-                                        direction: "flex",
-                                        justifyContent: "center",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        "&:hover": {
-                                            backgroundColor: "none",
-                                            border: "none !important",
-                                            color: "#2bb9a9",
-                                        },
-                                    }}
-                                    key={index}
-                                    href={category.href}
-                                >
-                                    {category.label}
-                                </Button>
-                            ))}
-                            <a href="/">
-                                {" "}
-                                {/* Adjust the href accordingly */}
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                        alignItems: "center",
-                                    }}
-                                >
-                                    <Image
-                                        loading="lazy"
-                                        width={100}
-                                        height={100}
-                                        src="https://res.cloudinary.com/dqhy9ufze/image/upload/v1715339533/Screenshot_2024-05-10_at_19.11.40-removebg-preview_zla5fj.png"
-                                        alt=""
-                                        className="cursor-pointer"
-                                    />
-                                    <Typography
-                                        sx={{
-                                            ...buttonTypo,
-                                            color: "#222222",
-                                        }}
-                                    >
-                                        {/* Text aligned with the image */}
-                                    </Typography>
-                                </div>
-                            </a>
+                  <Image
+                    loading="lazy"
+                    width={100}
+                    height={100}
+                    src="https://res.cloudinary.com/dqhy9ufze/image/upload/v1715339533/Screenshot_2024-05-10_at_19.11.40-removebg-preview_zla5fj.png"
+                    alt=""
+                    className="cursor-pointer"
+                  />
+                  <Typography
+                    sx={{
+                      ...buttonTypo,
+                      color: "#222222",
+                    }}
+                  >
+                    {/* Text aligned with the image */}
+                  </Typography>
+                </div>
+              </a>
 
-                            {categoriesRightSide.map((category, index) => (
-                                <Button
-                                    sx={{
-                                        ...buttonTypo,
-                                        direction: "flex",
-                                        justifyContent: "center",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        "&:hover": {
-                                            backgroundColor: "none",
-                                            border: "none !important",
-                                            color: "#2bb9a9",
-                                        },
-                                    }}
-                                    key={index}
-                                    href={category.href}
-                                >
-                                    {category.label}
-                                </Button>
-                            ))}
-                        </Stack>
-                    </Grid>
-                    <Grid item sx={{ width: "20%" }}></Grid>
-                </Grid>
+              {categoriesRightSide.map((category, index) => (
+                <Button
+                  sx={{
+                    ...buttonTypo,
+                    direction: "flex",
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "center",
+                    "&:hover": {
+                      backgroundColor: "none",
+                      border: "none !important",
+                      color: "#2bb9a9",
+                    },
+                  }}
+                  key={index}
+                  href={category.href}
+                >
+                  {category.label}
+                </Button>
+              ))}
+              <Grid item sx={{ width: "14%" }}>
+                <Stack
+                  direction={"row"}
+                  justifyContent={"flex-end"}
+                  sx={{ paddingRight: "48px" }}
+                >
+                  <Button
+                    sx={{
+                      padding: "0px",
+                      display: "flex",
+                      width: "fit-content",
+                    }}
+                    onClick={openAddCard}
+                  >
+                    <span style={{ color: "black", fontSize: "24px" }}>
+                      <AiOutlineShoppingCart />
+                    </span>
+                    <Typography
+                      sx={{
+                        ...gotaTypo,
+                        fontSize: "16px",
+                        px: "10px",
+                      }}
+                    >
+                      My Bag (2)
+                    </Typography>
+                  </Button>
+                </Stack>
+              </Grid>
             </Stack>
-        </>
-    );
+          </Grid>
+          <Grid item sx={{ width: "20%" }}></Grid>
+        </Grid>
+      </Stack>
+    </>
+  );
 }
 
 export default Header;

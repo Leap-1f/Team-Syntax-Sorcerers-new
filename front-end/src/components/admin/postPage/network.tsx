0b57a4951648f7411358001d941b644782d84333
Misheel burenzul <23LP7177@G401-CC0090.local>
2024-05-14 19:45:59 +0800

    fetched posts

--- front-end/src/components/admin/postPage/network.tsx
+++ front-end/src/components/admin/postPage/network.tsx
@@ -1,5 +1,18 @@
 export const getPosts = async () => {
-  const res = await fetch("http://localhost:8080/api/blog?limit=3", {
+  const res = await fetch("http://localhost:8080/api/blog?limit=1", {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  var zov = await res.json();
+
+  zov.status = res.status;
+  return zov;
+};
+export const getSinglePost = async () => {
+  const res = await fetch("http://localhost:8080/api/blog?limit=1", {
     mode: "cors",
     method: "GET",
     headers: {
--- front-end/src/components/blog/body.tsx
+++ front-end/src/components/blog/body.tsx
@@ -1,31 +1,103 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getPosts } from "../admin/postPage/network";
+import { getPosts, getSinglePost } from "../admin/postPage/network";
 import Image from "next/image";
 import { Box, Typography } from "@mui/material";
 
 const Body = () => {
   const [data, setData] = useState<any[]>([]);
-  async function refresh() {
+  const [sidePost, setSidePost] = useState<any[]>([]);
+  async function fetchThreePosts() {
     const mane = await getPosts();
+    console.log(mane);
+    setSidePost(mane);
+  }
+  async function fetchOnePost() {
+    const mane = await getSinglePost();
     console.log(mane);
     setData(mane);
   }
   useEffect(() => {
-    refresh();
+    fetchOnePost();
+    fetchThreePosts();
   }, []);
   return (
     <>
-      <Box></Box>
-      <Box sx={{display:"flex" , flexDirection:"column" , gap:"5rem" , justifyContent:"center" , alignItems:"center" , paddingBottom:"5rem"}}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "3rem",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingBottom: "5rem",
+        }}
+      >
         {data?.map((data, index) => {
           return (
             <>
-              <Box sx={{display:"flex", gap:"3rem"}}>
-                <Image src={data.image} width={300} height={300} alt="" style={{width:"250px", height:"250px"}}></Image>
-                <Box sx={{display:"flex" , flexDirection:"column",  width:"30rem" , gap:"2rem" , paddingTop:"2rem"}}>
-                  <Typography sx={{fontSize:"1.5rem" , fontWeight:"600"}}>{data.title}</Typography>
-                  <Typography sx={{}}>{data.description}</Typography>
+              <Box sx={{ display: "flex", gap: "2rem" }}>
+                <Box>
+                  <Box sx={{ border: "1px solid black" }}>
+                    <Image
+                      src={data.image}
+                      width={300}
+                      height={300}
+                      alt=""
+                      style={{ width: "500px", height: "500px" }}
+                    ></Image>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "2rem",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "1.5rem", fontWeight: "600" }}>
+                      {data.title}
+                    </Typography>
+                    <Typography sx={{ width: "500px" }}>
+                      {data.description}
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box
+                  sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+                >
+                  {sidePost?.map((sidePost, index) => {
+                    return (
+                      <>
+                        <Box sx={{ display: "flex", gap: "1rem" }}>
+                          <Box sx={{ border: "1px solid black" }}>
+                            <Image
+                              src={sidePost.image}
+                              width={300}
+                              height={300}
+                              alt=""
+                              style={{ width: "150px", height: "150px" }}
+                            ></Image>
+                          </Box>
+                          <Box
+                            sx={{
+                              display: "flex",
+                              flexDirection: "column",
+                              width: "30rem",
+                              gap: "1rem",
+                              paddingTop: "2rem",
+                            }}
+                          >
+                            <Typography
+                              sx={{ fontSize: "1.5rem", fontWeight: "600" }}
+                            >
+                              {sidePost.title}
+                            </Typography>
+                            <Typography>{sidePost.description}</Typography>
+                          </Box>
+                        </Box>
+                      </>
+                    );
+                  })}
                 </Box>
               </Box>
             </>
